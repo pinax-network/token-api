d381016906eb253f58e6138e7facf21db725bfaf
Etienne Donneger <etienne@pinax.network>
2025-03-16 20:12:35 -0400

    Remove outdated overrides for ClickHouse client
    The issue has been resolved https://github.com/oven-sh/bun/issues/5470

--- src/clickhouse/client.ts
+++ src/clickhouse/client.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@clickhouse/client-web";
-//import { ping } from "./ping.js";
 import { APP_NAME, config } from "../config.js";
 
 // TODO: Check how to abort previous queries if haven't returned yet
@@ -16,12 +15,6 @@ const client = (database?: string) => {
         application: APP_NAME,
     });
  
-    // These overrides should not be required but the @clickhouse/client-web instance
-    // does not work well with Bun's implementation of Node streams.
-    // https://github.com/oven-sh/bun/issues/5470
-    c.command = c.exec;
-    //c.ping = ping;
-
     return c;
 }
 
