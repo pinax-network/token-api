a6d697fbff2cc9f39cb8f45aca7954af44dc43f1
Etienne Donneger <etienne@pinax.network>
2025-03-27 15:22:40 +0000

    Improve `ohlc` SQL (#47)
    - Includes both directions for TOKEN/STABLE pairs
    - Weighted average for open/close based on pair prices count

--- src/sql/ohlcv_prices_usd_for_contract/evm.sql
+++ src/sql/ohlcv_prices_usd_for_contract/evm.sql
@@ -1,25 +1,77 @@
-SELECT
-    if(
-        toTime(toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)) = toDateTime('1970-01-02 00:00:00'),
-        toDate(toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)),
-        toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)
-    ) AS datetime,
-    CONCAT((SELECT symbol FROM contracts WHERE address = {contract: String}), 'USD') AS ticker,
-    argMinMerge(open) AS open,
-    if(
-        quantile({high_quantile: Float32})(high) < open,
-        open,
-        quantile({high_quantile: Float32})(high)
-    ) AS high,
-    if(
-        quantile({low_quantile: Float32})(low) > close,
-        close,
-        quantile({low_quantile: Float32})(low)
-    ) AS low,
-    argMaxMerge(close) AS close,
+WITH combined_prices AS
+(
+    SELECT
+        if(
+            toTime(toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)) = toDateTime('1970-01-02 00:00:00'),
+            toDate(toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)),
+            toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)
+        ) AS datetime,
+        CONCAT((SELECT symbol FROM contracts WHERE address = {contract: String}), 'USD') AS ticker,
+        argMinMerge(open) AS open,
+        multiIf(
+            quantile({high_quantile: Float32})(high) < open,
+            open,
+            quantile({high_quantile: Float32})(high) < close,
+            close,
+            quantile({high_quantile: Float32})(high)
+        ) AS high,
+        multiIf(
+            quantile({low_quantile: Float32})(low) > open,
+            open,
+            quantile({low_quantile: Float32})(low) > close,
+            close,
+            quantile({low_quantile: Float32})(low)
+        ) AS low,
+        argMaxMerge(close) AS close,
+        sum(volume) AS volume,
+        count(*) AS count
+    FROM ohlc_from_swaps
+    WHERE token1 = {contract: String} AND token0 IN {stablecoin_contracts: Array(String)}
+    GROUP BY datetime
+    HAVING datetime >= parseDateTimeBestEffortOrZero({min_datetime: String}) AND datetime <= parseDateTimeBestEffort({max_datetime: String})
+    ORDER BY datetime DESC
+
+    UNION ALL
+
+    SELECT
+        if(
+            toTime(toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)) = toDateTime('1970-01-02 00:00:00'),
+            toDate(toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)),
+            toStartOfInterval(datetime, INTERVAL {interval_minute: UInt64} MINUTE)
+        ) AS datetime,
+        CONCAT((SELECT symbol FROM contracts WHERE address = {contract: String}), 'USD') AS ticker,
+        1/argMinMerge(open) AS open,
+        multiIf(
+            quantile({high_quantile: Float32})(1/ohlc.low) < open,
+            open,
+            quantile({high_quantile: Float32})(1/ohlc.low) < close,
+            close,
+            quantile({high_quantile: Float32})(1/ohlc.low)
+        ) AS high,
+        multiIf(
+            quantile({low_quantile: Float32})(1/ohlc.high) > open,
+            open,
+            quantile({low_quantile: Float32})(1/ohlc.high) > close,
+            close,
+            quantile({low_quantile: Float32})(1/ohlc.high)
+        ) AS low,
+        1/argMaxMerge(close) AS close,
+        sum(volume) AS volume,
+        count(*) AS count
+    FROM ohlc_from_swaps AS ohlc
+    WHERE token0 = {contract: String} AND token1 IN {stablecoin_contracts: Array(String)}
+    GROUP BY datetime
+    HAVING datetime >= parseDateTimeBestEffortOrZero({min_datetime: String}) AND datetime <= parseDateTimeBestEffort({max_datetime: String})
+    ORDER BY datetime DESC
+)
+SELECT 
+    datetime,
+    ticker,
+    quantileExactWeighted(open, count) AS open,
+    max(high) AS high,
+    min(low) AS low,
+    quantileExactWeighted(close, count) AS close,
     sum(volume) AS volume
-FROM ohlc_from_swaps
-WHERE token1 = {contract: String} AND token0 IN {stablecoin_contracts: Array(String)}
-GROUP BY token1, datetime
-HAVING datetime >= parseDateTimeBestEffortOrZero({min_datetime: String}) AND datetime <= parseDateTimeBestEffort({max_datetime: String})
+FROM combined_prices
+GROUP BY datetime, ticker
 ORDER BY datetime DESC;
