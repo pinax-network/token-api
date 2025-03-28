f2899b13cf3e3f428b9b3199c86a2ddc16f8d0be
Denis Carriere <carriere.denis@gmail.com>
2025-03-27 20:39:18 -0400

    Handle wrapped tokens as well (WETH => ETH, WBNB => BNB, etc)

--- src/inject/icon.ts
+++ src/inject/icon.ts
@@ -6,7 +6,8 @@ type Data = { symbol: string, icon: { web3icon: string; }; };
 export function injectIcons(response: ApiUsageResponse|ApiErrorResponse) {
     if ('data' in response) {
         response.data.forEach((row: Data) => {
-            const web3icon = findIcon(row.symbol);
+            // Handle wrapped tokens as well (WETH => ETH, WBNB => BNB, etc)
+            const web3icon = findIcon(row.symbol) ?? findIcon(row.symbol.replace(/^W/, ""));
             if (web3icon) {
                 row.icon = {
                     web3icon
