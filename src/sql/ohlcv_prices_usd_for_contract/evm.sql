b016f272e0507333958d1824e2e8f0a9d504d10c
Etienne Donneger <etienne@pinax.network>
2025-04-22 12:18:05 -0400

    Update OHLCV usd SQL
    Combine both swap directions into single CTE tables

--- src/sql/ohlcv_prices_usd_for_contract/evm.sql
+++ src/sql/ohlcv_prices_usd_for_contract/evm.sql
@@ -1,119 +1,126 @@
 WITH 
-metadata_01 AS (
+pool_metadata AS (
     SELECT
         p.pool,
-        t0.symbol AS symbol,
-        t0.decimals AS t0_decimals,
-        t1.decimals AS t1_decimals
+        p.token0, p.token1,
+        t0.symbol AS token0_symbol,
+        t1.symbol AS token1_symbol,
+        t0.decimals AS token0_decimals,
+        t1.decimals AS token1_decimals,
+        CASE 
+            WHEN p.token0 = {contract: String} THEN 0
+            WHEN p.token1 = {contract: String} THEN 1
+            ELSE NULL
+        END AS weth_position
     FROM pools AS p
     LEFT JOIN contracts AS t0 FINAL ON p.token0 = t0.address
     LEFT JOIN contracts AS t1 FINAL ON p.token1 = t1.address
-    WHERE isNotNull(symbol) AND isNotNull(t0_decimals) AND isNotNull(t1_decimals)
-    AND token0 = {contract: String}
-    AND token1 IN {stablecoin_contracts: Array(String)}
+    WHERE 
+        isNotNull(t0.decimals) AND isNotNull(t1.decimals) AND
+        (p.token0 = {contract: String} OR p.token1 = {contract: String}) AND
+        (p.token0 IN {stablecoin_contracts: Array(String)} OR p.token1 IN {stablecoin_contracts: Array(String)})
 ),
-metadata_10 AS (
-    SELECT
-        p.pool,
-        t0.decimals AS t0_decimals,
-        t1.decimals AS t1_decimals
-    FROM pools AS p
-    LEFT JOIN contracts AS t0 FINAL ON p.token0 = t0.address
-    LEFT JOIN contracts AS t1 FINAL ON p.token1 = t1.address
-    WHERE isNotNull(t0_decimals) AND isNotNull(t1_decimals)
-    AND token1 = {contract: String}
-    AND token0 IN {stablecoin_contracts: Array(String)}
-),
-ohlc_10 AS (
+unified_ohlc AS (
     SELECT
         if(
-            toTime(toStartOfInterval(timestamp, INTERVAL {interval_minute: UInt64} MINUTE)) = toDateTime('1970-01-02 00:00:00'),
-            toDate(toStartOfInterval(timestamp, INTERVAL {interval_minute: UInt64} MINUTE)),
-            toStartOfInterval(timestamp, INTERVAL {interval_minute: UInt64} MINUTE)
+            toTime(toStartOfInterval(o.timestamp, INTERVAL {interval_minute: UInt64} MINUTE)) = toDateTime('1970-01-02 00:00:00'),
+            toDate(toStartOfInterval(o.timestamp, INTERVAL {interval_minute: UInt64} MINUTE)),
+            toStartOfInterval(o.timestamp, INTERVAL {interval_minute: UInt64} MINUTE)
         ) AS datetime,
-        1/argMinMerge(open0) AS open_raw,
-        1/quantileDeterministicMerge({low_quantile: Float32})(low0) AS high_raw,
-        1/quantileDeterministicMerge({high_quantile: Float32})(high0) AS low_raw,
-        1/argMaxMerge(close0) AS close_raw,
-        sum(gross_volume0) AS volume,
-        sum(transactions) AS transactions,
-        t0_decimals, t1_decimals
-    FROM ohlc_prices AS o_01
-    INNER JOIN metadata_10 AS m ON o_01.pool = m.pool
-    GROUP BY datetime, t0_decimals, t1_decimals
-    HAVING datetime >= parseDateTimeBestEffortOrZero({min_datetime: String}) 
-       AND datetime <= parseDateTimeBestEffort({min_datetime: String})
-    ORDER BY datetime DESC
+        m.weth_position,
+        m.token0_symbol,
+        m.token1_symbol,
+        m.token0_decimals,
+        m.token1_decimals,
+        CASE 
+            WHEN m.weth_position = 0 THEN argMinMerge(o.open0)
+            WHEN m.weth_position = 1 THEN 1/argMinMerge(o.open0)
+        END AS open_raw,
+        CASE 
+            WHEN m.weth_position = 0 THEN quantileDeterministicMerge({high_quantile: Float32})(o.high0)
+            WHEN m.weth_position = 1 THEN 1/quantileDeterministicMerge({low_quantile: Float32})(o.low0)
+        END AS high_raw,
+        CASE 
+            WHEN m.weth_position = 0 THEN quantileDeterministicMerge({low_quantile: Float32})(o.low0)
+            WHEN m.weth_position = 1 THEN 1/quantileDeterministicMerge({high_quantile: Float32})(o.high0)
+        END AS low_raw,
+        CASE 
+            WHEN m.weth_position = 0 THEN argMaxMerge(o.close0)
+            WHEN m.weth_position = 1 THEN 1/argMaxMerge(o.close0)
+        END AS close_raw,
+        CASE
+            WHEN m.weth_position = 0 THEN sum(o.gross_volume1)
+            WHEN m.weth_position = 1 THEN sum(o.gross_volume0)
+        END AS volume,
+        uniqMerge(o.uaw) AS uaw,
+        sum(o.transactions) AS transactions
+    FROM ohlc_prices AS o
+    INNER JOIN pool_metadata AS m ON o.pool = m.pool
+    WHERE o.timestamp >= parseDateTimeBestEffortOrZero({min_datetime: String}) 
+      AND o.timestamp <= parseDateTimeBestEffort({max_datetime: String})
+    GROUP BY datetime, m.weth_position, m.token0_symbol, m.token1_symbol, m.token0_decimals, m.token1_decimals
 ),
-ohlc_01 AS (
-    SELECT
-        if(
-            toTime(toStartOfInterval(timestamp, INTERVAL {interval_minute: UInt64} MINUTE)) = toDateTime('1970-01-02 00:00:00'),
-            toDate(toStartOfInterval(timestamp, INTERVAL {interval_minute: UInt64} MINUTE)),
-            toStartOfInterval(timestamp, INTERVAL {interval_minute: UInt64} MINUTE)
-        ) AS datetime,
-        argMinMerge(open0) AS open_raw,
-        quantileDeterministicMerge({high_quantile: Float32})(high0) AS high_raw,
-        quantileDeterministicMerge({high_quantile: Float32})(low0) AS low_raw,
-        argMaxMerge(close0) AS close_raw,
-        sum(gross_volume1) AS volume,
-        sum(transactions) AS transactions,
-        t0_decimals, t1_decimals
-    FROM ohlc_prices AS o_10
-    INNER JOIN metadata_01 AS m ON o_10.pool = m.pool
-    GROUP BY datetime, t0_decimals, t1_decimals
-    HAVING datetime >= parseDateTimeBestEffortOrZero({min_datetime: String}) 
-       AND datetime <= parseDateTimeBestEffort({max_datetime: String})
-    ORDER BY datetime DESC
-),
-ohlc AS (
+normalized_prices AS (
     SELECT
         datetime,
-        open_raw * pow(10, t0_decimals - t1_decimals) AS open,
-        multiIf(
-            high_raw < open_raw, open_raw,
-            high_raw < close_raw, close_raw,
-            high_raw
-        ) * pow(10, t0_decimals - t1_decimals) AS high,
-        multiIf(
-            low_raw > open_raw, open_raw,
-            low_raw > close_raw, close_raw,
-            low_raw
-        ) * pow(10, t0_decimals - t1_decimals) AS low,
-        close_raw * pow(10, t0_decimals - t1_decimals) AS close,
-        toFloat64(volume) * pow(10, -t1_decimals) AS volume,
+        CASE
+            WHEN weth_position = 0 THEN token1_symbol
+            WHEN weth_position = 1 THEN token0_symbol
+        END AS stable_symbol,
+        CASE
+            WHEN weth_position = 0 THEN open_raw * pow(10, token0_decimals - token1_decimals)
+            WHEN weth_position = 1 THEN open_raw * pow(10, token1_decimals - token0_decimals)
+        END AS open,
+        CASE
+            WHEN weth_position = 0 THEN 
+                multiIf(
+                    high_raw < open_raw, open_raw,
+                    high_raw < close_raw, close_raw,
+                    high_raw
+                ) * pow(10, token0_decimals - token1_decimals)
+            WHEN weth_position = 1 THEN 
+                multiIf(
+                    high_raw < open_raw, open_raw,
+                    high_raw < close_raw, close_raw,
+                    high_raw
+                ) * pow(10, token1_decimals - token0_decimals)
+        END AS high,
+        CASE
+            WHEN weth_position = 0 THEN 
+                multiIf(
+                    low_raw > open_raw, open_raw,
+                    low_raw > close_raw, close_raw,
+                    low_raw
+                ) * pow(10, token0_decimals - token1_decimals)
+            WHEN weth_position = 1 THEN 
+                multiIf(
+                    low_raw > open_raw, open_raw,
+                    low_raw > close_raw, close_raw,
+                    low_raw
+                ) * pow(10, token1_decimals - token0_decimals)
+        END AS low,
+        CASE
+            WHEN weth_position = 0 THEN close_raw * pow(10, token0_decimals - token1_decimals)
+            WHEN weth_position = 1 THEN close_raw * pow(10, token1_decimals - token0_decimals)
+        END AS close,
+        CASE
+            WHEN weth_position = 0 THEN toFloat64(volume) * pow(10, -token1_decimals)
+            WHEN weth_position = 1 THEN toFloat64(volume) * pow(10, -token0_decimals)
+        END AS volume,
+        uaw,
         transactions
-    FROM ohlc_01
-
-    UNION ALL
-
-    SELECT
-        datetime,
-        open_raw * pow(10, t1_decimals - t0_decimals) AS open,
-        multiIf(
-            high_raw < open_raw, open_raw,
-            high_raw < close_raw, close_raw,
-            high_raw
-        ) * pow(10, t1_decimals - t0_decimals) AS high,
-        multiIf(
-            low_raw > open_raw, open_raw,
-            low_raw > close_raw, close_raw,
-            low_raw
-        ) * pow(10, t1_decimals - t0_decimals) AS low,
-        close_raw * pow(10, t1_decimals - t0_decimals) AS close,
-        toFloat64(volume) * pow(10, -t0_decimals) AS volume,
-        transactions
-    FROM ohlc_10
+    FROM unified_ohlc
 )
 SELECT
     datetime,
-    CONCAT((SELECT symbol FROM metadata_01 GROUP BY symbol), 'USD') AS ticker,
-    quantileExactWeighted(open, ohlc.transactions) AS open,
-    quantileExactWeighted(high, ohlc.transactions) AS high,
-    quantileExactWeighted(low, ohlc.transactions) AS low,
-    quantileExactWeighted(close, ohlc.transactions) AS close,
+    CONCAT((SELECT token0_symbol FROM pool_metadata WHERE weth_position = 0 LIMIT 1), 'USD') AS ticker,
+    quantileExactWeighted(open, normalized_prices.transactions) AS open,
+    quantileExactWeighted(high, normalized_prices.transactions) AS high,
+    quantileExactWeighted(low, normalized_prices.transactions) AS low,
+    quantileExactWeighted(close, normalized_prices.transactions) AS close,
     sum(volume) AS volume,
+    sum(uaw) AS uaw,
     sum(transactions) AS transactions
-FROM ohlc
+FROM normalized_prices
 GROUP BY datetime, ticker
 ORDER BY datetime DESC;
