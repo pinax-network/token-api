dc4d02e8cc77d0a390f4836c204eace320acba39
Etienne Donneger <etienne@pinax.network>
2025-05-19 19:44:08 -0400

    Add single NFT token lookup endpoint
    Includes NFT data, metadata, last sale and last collection sale.

--- src/routes/nft/index.ts
+++ src/routes/nft/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono';
 import metadata from "./metadata/index.js";
+import token from "./token/index.js";
 
 const router = new Hono();
 
 router.route('/metadata', metadata);
+router.route('/token', token);
 
 export default router;
