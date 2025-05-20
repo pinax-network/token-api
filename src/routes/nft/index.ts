00c8e5faa5f1f6326c1d076d2b148879c26ec2f8
Etienne Donneger <etienne@pinax.network>
2025-05-19 22:50:22 -0400

    Add NFT collection transfers endpoint

--- src/routes/nft/index.ts
+++ src/routes/nft/index.ts
@@ -1,12 +1,14 @@
 import { Hono } from 'hono';
 import metadata from "./metadata/index.js";
 import token from "./token/index.js";
+import transfers_collection from "./transfers_collection/index.js";
 import wallet from "./wallet/index.js";
 
 const router = new Hono();
 
 router.route('/metadata', metadata);
 router.route('/token', token);
+router.route('/transfers_collection', transfers_collection);
 router.route('/wallet', wallet);
 
 export default router;
