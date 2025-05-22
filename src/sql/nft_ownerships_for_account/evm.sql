c2dcbe38c05c49496bc83912a424f3a933f8fac1
Etienne Donneger <etienne@pinax.network>
2025-05-22 11:10:18 -0400

    Add ERC1155 for NFT ownerships

--- src/sql/nft_ownerships_for_account/evm.sql
+++ src/sql/nft_ownerships_for_account/evm.sql
@@ -1,21 +1,49 @@
-WITH ownerships AS (
-    SELECT
-        token_id,
-        'ERC721' AS token_standard,
-        contract,
-        owner,
-    FROM erc721_owners
-    WHERE owner = lower({address: String})
+WITH erc721 AS (
+    SELECT DISTINCT
+        o.token_id,
+        o.token_standard,
+        o.contract,
+        o.owner AS owner,
+        m.symbol,
+        m.name,
+        {network_id:String} as network_id
+    FROM (
+        SELECT
+            token_id,
+            'ERC721' AS token_standard,
+            contract,
+            owner,
+        FROM erc721_owners
+        WHERE owner = {address: String}
+    ) AS o
+    LEFT JOIN erc721_metadata_by_contract AS m USING (contract)
+),
+erc1155 AS (
+    SELECT DISTINCT
+        o.token_id,
+        o.token_standard,
+        o.contract,
+        o.owner AS owner,
+        m.symbol,
+        m.name,
+        {network_id:String} as network_id
+    FROM (
+        SELECT
+            token_id,
+            'ERC1155' AS token_standard,
+            contract,
+            owner,
+        FROM erc1155_balances
+        WHERE owner = {address: String} AND balance > 0
+    ) AS o
+    LEFT JOIN erc1155_metadata_by_contract AS m USING (contract)
+),
+combined AS (
+    SELECT * FROM erc721
+    UNION ALL
+    SELECT * FROM erc1155
 )
-SELECT
-    o.token_id,
-    o.token_standard,
-    o.contract,
-    o.owner AS owner,
-    m.symbol,
-    m.name,
-    {network_id:String} as network_id
-FROM ownerships AS o
-JOIN erc721_metadata_by_contract AS m USING (contract)
+SELECT * FROM combined
+ORDER BY token_standard, contract, token_id
 LIMIT {limit:int}
 OFFSET {offset:int}
