123bab4ebf235d855c743fb19d00874a70ff1c5d
Denis Carriere <carriere.denis@gmail.com>
2025-05-21 22:17:42 -0400

    add offset

--- src/sql/nft_holders/evm.sql
+++ src/sql/nft_holders/evm.sql
@@ -1,11 +1,8 @@
 WITH erc721 AS (
     WITH (
-        SELECT
-            count()
-        FROM
-            erc721_owners FINAL
-        WHERE
-            contract = { contract: String }
+        SELECT count()
+        FROM erc721_owners FINAL
+        WHERE contract = { contract: String }
     ) AS total_supply
     SELECT
         'ERC721' AS token_standard,
@@ -14,24 +11,16 @@ WITH erc721 AS (
         quantity AS quantity_unique,
         quantity / total_supply AS percentage,
         { network_id :String } as network_id
-    FROM
-        erc721_owners FINAL
-    WHERE
-        contract = { contract: String }
-    GROUP BY
-        owner
-    ORDER BY
-        count() DESC
+    FROM erc721_owners FINAL
+    WHERE contract = { contract: String }
+    GROUP BY owner
+    ORDER BY count() DESC
 ),
 erc1155 AS (
     WITH (
-        SELECT
-            sum(balance) as supply
-        FROM
-            erc1155_balances FINAL
-        WHERE
-            contract = { contract: String }
-            AND balance > 0
+        SELECT sum(balance) as supply
+        FROM erc1155_balances FINAL
+        WHERE contract = { contract: String } AND balance > 0
     ) AS total_supply
     SELECT
         'ERC1155' AS token_standard,
@@ -40,29 +29,17 @@ erc1155 AS (
         uniq(token_id) AS quantity_unique,
         quantity / total_supply AS percentage,
         { network_id :String } as network_id
-    FROM
-        erc1155_balances FINAL
-    WHERE
-        contract = { contract: String }
-        AND balance > 0
-    GROUP BY
-        owner
-    ORDER BY
-        quantity DESC
+    FROM erc1155_balances FINAL
+    WHERE contract = { contract: String } AND balance > 0
+    GROUP BY owner
+    ORDER BY quantity DESC
 ),
 combined AS (
-    SELECT
-        *
-    FROM
-        erc721
-    UNION
-    ALL
-    SELECT
-        *
-    FROM
-        erc1155
+    SELECT * FROM erc721
+    UNION ALL
+    SELECT * FROM erc1155
 )
-SELECT
-    *
-FROM
-    combined
+SELECT *
+FROM combined
+LIMIT {limit:int}
+OFFSET {offset:int}
