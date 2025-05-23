caaf027f10d11a906bddcd039dac3423368c4959
Etienne Donneger <etienne@pinax.network>
2025-05-23 11:44:20 -0400

    Optimize SQL for NFT collection

--- src/sql/nft_metadata_for_collection/evm.sql
+++ src/sql/nft_metadata_for_collection/evm.sql
@@ -1,16 +1,13 @@
 WITH erc721 AS (
     SELECT
         m.contract AS contract,
-        c.timestamp AS contract_creation,
-        c.creator AS contract_creator,
         'ERC721' AS token_standard,
-        m.symbol,
         m.name,
+        m.symbol,
         (
             SELECT
                 count(token_id)
             FROM erc721_owners
-            FINAL
             WHERE contract = {contract: String}
         ) AS total_supply,
         total_supply AS total_unique_supply,
@@ -18,72 +15,85 @@ WITH erc721 AS (
             SELECT
                 uniq(owner)
             FROM erc721_owners
-            FINAL
             WHERE contract = {contract: String}
         ) AS owners,
         (
             SELECT
                 count(*)
             FROM erc721_transfers
-            FINAL
             WHERE contract = {contract: String}
         ) AS total_transfers,
         {network_id:String} as network_id
     FROM erc721_metadata_by_contract AS m
     FINAL
-    LEFT JOIN `mainnet:evm-contracts@v0.3.1`.contracts AS c ON c.address = m.contract
     WHERE m.contract = {contract: String}
 ),
 erc1155_metadata_by_contract AS (
     SELECT DISTINCT
-        contract
+        contract,
+        'TO IMPLEMENT OFFCHAIN' AS name,
+        'TO IMPLEMENT OFFCHAIN' AS symbol
     FROM erc1155_balances
     FINAL
+    WHERE contract = {contract:String} AND balance > 0
 ),
 erc1155 AS (
     SELECT
         {contract: String} AS contract,
-        c.timestamp AS contract_creation,
-        c.creator AS contract_creator,
         'ERC1155' AS token_standard,
-        'TO IMPLEMENT OFFCHAIN' AS symbol,
-        'TO IMPLEMENT OFFCHAIN' AS name,
+        m.name,
+        m.symbol,
         (
             SELECT
                 count(token_id)
             FROM erc1155_balances
-            FINAL
             WHERE contract = {contract: String} AND balance > 0
         ) AS total_supply,
         (
             SELECT
                 uniq(token_id)
             FROM erc1155_balances
-            FINAL
             WHERE contract = {contract: String} AND balance > 0
         ) AS total_unique_supply,
         (
             SELECT
                 uniq(owner)
             FROM erc1155_balances
-            FINAL
             WHERE contract = {contract: String} AND balance > 0
         ) AS owners,
         (
             SELECT
                 count(*)
             FROM erc1155_transfers
-            FINAL
             WHERE contract = {contract: String}
         ) AS total_transfers,
         {network_id:String} as network_id
     FROM erc1155_metadata_by_contract AS m
-    LEFT JOIN `mainnet:evm-contracts@v0.3.1`.contracts AS c ON c.address = m.contract
     WHERE m.contract = {contract: String}
 ),
 combined AS (
     SELECT * FROM erc721
     UNION ALL
     SELECT * FROM erc1155
+),
+contract_creation AS (
+    SELECT
+        address AS contract,
+        timestamp,
+        creator
+    FROM `mainnet:evm-contracts@v0.3.1`.contracts
+    WHERE address = {contract: String}
 )
-SELECT * FROM combined
+SELECT
+    contract,
+    timestamp AS contract_creation,
+    creator AS contract_creator,
+    name,
+    symbol,
+    owners,
+    total_supply,
+    total_unique_supply,
+    total_transfers,
+    network_id
+FROM combined
+LEFT JOIN contract_creation USING (contract)
