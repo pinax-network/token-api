4099d9ce1531991661816b238324dd7190622189
Etienne Donneger <etienne@pinax.network>
2025-05-23 10:50:40 -0400

    Add MINT/BURN to NFT activites and optimize SQL

--- src/sql/nft_activities/evm.sql
+++ src/sql/nft_activities/evm.sql
@@ -1,53 +1,61 @@
 WITH erc721 AS (
     SELECT
-        'TRANSFER' AS "@type",
+        CASE
+            WHEN lower(from) IN (
+                '0x0000000000000000000000000000000000000000',
+                '0x000000000000000000000000000000000000dead'
+            ) THEN 'MINT'
+            WHEN lower(to) IN (
+                '0x0000000000000000000000000000000000000000',
+                '0x000000000000000000000000000000000000dead'
+            ) THEN 'BURN'
+            ELSE 'TRANSFER'
+        END AS "@type",
         block_num,
         block_hash,
         timestamp,
         tx_hash,
         contract,
-        symbol,
-        name,
         from,
         to,
-        toString(token_id) AS token_id,
+        token_id,
         amount,
         transfer_type,
         token_standard
     FROM erc721_transfers AS t
-    LEFT JOIN erc721_metadata_by_contract AS m USING (contract)
-    WHERE   timestamp BETWEEN {startTime:UInt32} AND {endTime:UInt32}
-        AND ({contract:String}          = '' OR contract       = {contract:String})
+    WHERE timestamp BETWEEN {startTime:UInt32} AND {endTime:UInt32}
+        AND contract = {contract:String}
         AND ({fromAddress:String}       = '' OR from           = {fromAddress:String})
         AND ({toAddress:String}         = '' OR to             = {toAddress:String})
         AND ({anyAddress:String}        = '' OR (to = {anyAddress:String} OR from = {anyAddress:String}))
 ),
-erc1155_metadata_by_contract AS (
-    SELECT DISTINCT
-        contract
-    FROM erc1155_balances
-    FINAL
-),
 erc1155 AS (
     SELECT
-        'TRANSFER' AS "@type",
+        CASE
+            WHEN lower(from) IN (
+                '0x0000000000000000000000000000000000000000',
+                '0x000000000000000000000000000000000000dead'
+            ) THEN 'MINT'
+            WHEN lower(to) IN (
+                '0x0000000000000000000000000000000000000000',
+                '0x000000000000000000000000000000000000dead'
+            ) THEN 'BURN'
+            ELSE 'TRANSFER'
+        END AS "@type",
         block_num,
         block_hash,
         timestamp,
         tx_hash,
         contract,
-        'TO IMPLEMENT OFFCHAIN' AS symbol,
-        'TO IMPLEMENT OFFCHAIN' AS name,
         from,
         to,
-        toString(token_id) AS token_id,
+        token_id,
         amount,
         transfer_type,
         token_standard
     FROM erc1155_transfers AS t
-    LEFT JOIN erc1155_metadata_by_contract AS m USING (contract)
-    WHERE   timestamp BETWEEN {startTime:UInt32} AND {endTime:UInt32}
-        AND ({contract:String}          = '' OR contract       = {contract:String})
+    WHERE timestamp BETWEEN {startTime:UInt32} AND {endTime:UInt32}
+        AND contract = {contract:String}
         AND ({fromAddress:String}       = '' OR from           = {fromAddress:String})
         AND ({toAddress:String}         = '' OR to             = {toAddress:String})
         AND ({anyAddress:String}        = '' OR (to = {anyAddress:String} OR from = {anyAddress:String}))
@@ -56,8 +64,47 @@ combined AS (
     SELECT * FROM erc721
     UNION ALL
     SELECT * FROM erc1155
+),
+limit_combined AS (
+    SELECT *
+    FROM combined
+    ORDER BY timestamp DESC
+    LIMIT {limit:int}
+    OFFSET {offset:int}
+),
+erc721_metadata_by_contract AS (
+    SELECT DISTINCT
+        contract,
+        name,
+        symbol
+    FROM erc721_metadata_by_contract
+    WHERE contract = {contract:String}
+),
+erc1155_metadata_by_contract AS (
+    SELECT DISTINCT
+        contract,
+        token_id,
+        name,
+        '' AS symbol
+    FROM nft_metadata
+    WHERE contract = {contract:String}
 )
-SELECT * FROM combined
-ORDER BY timestamp DESC
-LIMIT {limit:int}
-OFFSET {offset:int}
+SELECT
+    `@type`,
+    c.block_num,
+    c.block_hash,
+    c.timestamp,
+    tx_hash,
+    token_standard,
+    contract,
+    if(length(m.name) > 0, m.name, m2.name) AS name,
+    if(length(m.symbol) > 0, m.symbol, m2.symbol) AS symbol,
+    from,
+    to,
+    toString(token_id) AS token_id,
+    amount,
+    transfer_type
+FROM limit_combined AS c
+LEFT JOIN erc721_metadata_by_contract AS m USING (contract)
+LEFT JOIN erc1155_metadata_by_contract AS m2 USING (contract, token_id)
+ORDER BY c.timestamp DESC
