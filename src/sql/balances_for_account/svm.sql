d50d288288bb8e1f9ccb89192369b075fdb15939
Denis Carriere <carriere.denis@gmail.com>
2025-07-10 01:46:08 -0400

    fix balances

--- src/sql/balances_for_account/svm.sql
+++ src/sql/balances_for_account/svm.sql
@@ -1,18 +1,27 @@
+WITH b AS (
+    SELECT
+        *,
+        row_number() OVER (
+            PARTITION BY owner, mint
+            ORDER BY block_num DESC
+        ) AS rn
+    FROM balances
+    WHERE owner = {token_account:String}
+      AND ({mint:String} = '' OR mint = {mint:String})
+)
 SELECT
     block_num,
-    b.timestamp AS datetime,
-    toUnixTimestamp(b.timestamp) AS timestamp,
-    toString(program_id) AS program_id,
-    toString(owner) AS token_account,
-    toString(mint) AS mint,
-    toString(b.amount) as amount,
-    b.amount / pow(10, decimals) as value,
+    b.timestamp                         AS datetime,
+    toUnixTimestamp(b.timestamp)        AS timestamp,
+    toString(program_id)                AS program_id,
+    toString(owner)                     AS token_account,
+    toString(mint)                      AS mint,
+    toString(b.amount)                  AS amount,
+    b.amount / pow(10, decimals)        AS value,
     decimals,
-    {network_id: String} as network_id
-FROM balances AS b
-WHERE
-    owner = {token_account:String}
-    AND ({mint: String} = '' OR mint = {mint: String})
-ORDER BY timestamp DESC
-LIMIT {limit:int}
-OFFSET {offset:int}
+    {network_id:String}     AS network_id
+FROM b
+WHERE rn = 1
+ORDER BY block_num DESC
+LIMIT  {limit:Int}
+OFFSET {offset:Int};
