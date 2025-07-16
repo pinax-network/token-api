771f10beadb686128f6852d08ac5d804eca7918f
Etienne Donneger <etienne@pinax.network>
2025-07-16 11:57:11 +0000

    Fix EVM holders SQL (#126)
    Replace `FINAL` with `argMax` aggregations

--- src/sql/holders_for_contract/evm.sql
+++ src/sql/holders_for_contract/evm.sql
@@ -1,18 +1,28 @@
+WITH filtered_balances AS (
+    SELECT
+        max(block_num) AS block_num,
+        max(timestamp) AS timestamp,
+        address,
+        contract,
+        toString(argMax(balance_raw, b.timestamp)) AS amount,
+        argMax(balance, b.timestamp) AS value
+    FROM balances_by_contract AS b
+    WHERE
+        contract = {contract: String} AND balance_raw > 0
+    GROUP BY contract, address
+    ORDER BY value DESC
+    LIMIT   {limit:int}
+    OFFSET  {offset:int}
+)
 SELECT
     block_num,
     timestamp as last_balance_update,
     address,
-    toString(balance_raw) AS amount,
-    balance as value,
+    amount,
+    value,
+    name,
     decimals,
     trim(symbol) as symbol,
     {network_id: String} as network_id
-FROM balances_by_contract
-FINAL
-JOIN erc20_metadata_initialize
-    ON balances_by_contract.contract = erc20_metadata_initialize.address
-WHERE
-    contract = {contract: String} AND balance_raw > 0
-ORDER BY value DESC
-LIMIT   {limit:int}
-OFFSET  {offset:int}
+FROM filtered_balances AS a
+JOIN erc20_metadata_initialize AS b ON a.contract = b.address
