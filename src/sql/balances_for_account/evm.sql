b8df105cbc1c38d763388b7c6063136c839eb6cc
Etienne Donneger <etienne@pinax.network>
2025-07-17 09:28:01 -0400

    Improve SQL for EVM balances for account
    Move filtering in subquery to avoid costly `JOIN`.

--- src/sql/balances_for_account/evm.sql
+++ src/sql/balances_for_account/evm.sql
@@ -1,17 +1,32 @@
+WITH filtered_balances AS (
+    SELECT
+        max(block_num) AS block_num,
+        max(timestamp) AS timestamp,
+        address,
+        contract,
+        toString(argMax(balance_raw, b.timestamp)) AS amount,
+        argMax(balance, b.timestamp) AS value,
+        name,
+        symbol,
+        decimals
+    FROM balances AS b
+    WHERE
+        (address = {address: String} AND balance_raw > 0)
+        AND ({contract: String} = '' OR contract = {contract: String})
+    GROUP BY address, contract, name, symbol, decimals
+    ORDER BY timestamp DESC
+    LIMIT   {limit:int}
+    OFFSET  {offset:int}
+)
 SELECT
-    max(block_num) AS block_num,
-    max(timestamp) AS datetime,
-    toString(contract) AS contract,
-    toString(argMax(balance_raw, timestamp)) AS amount,
-    argMax(balance, timestamp) as value,
-    decimals,
-    trim(symbol) as symbol,
+    block_num,
+    timestamp as last_balance_update,
+    toString(contract) as contract,
+    amount,
+    value,
+    COALESCE(b.name, a.name) as name,
+    trim(COALESCE(b.symbol, a.symbol)) as symbol,
+    COALESCE(b.decimals, a.decimals) as decimals,
     {network_id: String} as network_id
-FROM balances
-WHERE
-    (address = {address: String} AND balance_raw > 0)
-    AND ({contract: String} = '' OR contract = {contract: String})
-GROUP BY contract, symbol, decimals
-ORDER BY datetime DESC
-LIMIT   {limit:int}
-OFFSET  {offset:int}
+FROM filtered_balances AS a
+LEFT JOIN erc20_metadata_initialize AS b ON a.contract = b.address
