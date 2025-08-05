0b970129e7428ca04d3d56e38917914a34ed4d32
Etienne Donneger <etienne@pinax.network>
2025-08-05 04:36:12 -0400

    Add SVM Liquidity pools endpoint

--- src/sql/pools/svm.sql
+++ src/sql/pools/svm.sql
@@ -1,36 +1,41 @@
 WITH filtered_pools AS (
     SELECT
-        block_num,
-        timestamp as datetime,
-        signature AS transaction_id,
-        toString(user) AS creator,
-        toString(amm) AS pool,
-        coin_mint,
-        pc_mint
-    FROM raydium_amm_v4_initialize AS i
+        program_id,
+        program_name,
+        amm,
+        amm_name,
+        amm_pool,
+        mint0,
+        mint0_name,
+        mint1,
+        mint1_name,
+        transactions
+    FROM pool_activity_summary
     WHERE
-        if ({pool:String} == '', true, amm = {pool:String}) AND
-        if ({creator:String} == '', true, user = {creator:String}) AND
-        if ({token:String} == '', true, pc_mint = {token:String} OR coin_mint = {token:String})
+        if ({program_id:String} == '', true, program_id = {program_id:String}) AND
+        if ({amm:String} == '', true, amm = {amm:String}) AND
+        if ({amm_pool:String} == '', true, amm_pool = {amm_pool:String}) AND
+        if ({input_mint:String} == '', true, mint1 = {input_mint:String}) AND
+        if ({output_mint:String} == '', true, mint0 = {output_mint:String})
 )
 SELECT
-    pools.block_num AS block_num,
-    datetime,
-    transaction_id,
-    creator,
-    pool,
+    toString(program_id) AS program_id,
+    toString(program_name) AS program_name,
+    toString(amm) AS amm,
+    toString(amm_name) AS amm_name,
+    toString(amm_pool) AS amm_pool,
     CAST(
-        ( toString(c0.mint), 'TO IMPLEMENT', c0.decimals )
-        AS Tuple(address String, symbol  String, decimals UInt8)
-    ) AS token0,
+        ( toString(mint1), toString(mint1_name) )
+        AS Tuple(address String, symbol  String)
+    ) AS input_mint,
     CAST(
-        ( toString(c1.mint), 'TO IMPLEMENT', c1.decimals )
-        AS Tuple(address String, symbol  String, decimals UInt8)
-    ) AS token1,
+        ( toString(mint0), toString(mint0_name) )
+        AS Tuple(address String, symbol  String)
+    ) AS output_mint,
+    sum(transactions) AS transactions,
     {network_id: String} as network_id
 FROM filtered_pools AS pools
-JOIN mints AS c0 ON c0.mint = pools.coin_mint
-JOIN mints AS c1 ON c1.mint = pools.pc_mint
-ORDER BY datetime DESC
+GROUP BY program_id, program_name, amm, amm_name, amm_pool, input_mint, output_mint
+ORDER BY amm, transactions DESC, amm_pool
 LIMIT   {limit:int}
 OFFSET  {offset:int}
