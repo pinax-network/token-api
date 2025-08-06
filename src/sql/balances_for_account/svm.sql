c03fdac38a76398824014f1375cd8927a60c4a59
Etienne Donneger <etienne@pinax.network>
2025-08-06 09:14:25 -0400

    Add SVM token metadata to balances and transfers endpoints
    New fields `name` and `symbol` joined from new table `metadata`
    in Solana tokens databases.

--- src/sql/balances_for_account/svm.sql
+++ src/sql/balances_for_account/svm.sql
@@ -1,19 +1,38 @@
 SELECT
     block_num,
-    b.timestamp                         AS datetime,
-    toUnixTimestamp(b.timestamp)        AS timestamp,
-    toString(program_id)                AS program_id,
-    toString(owner)                     AS token_account,
-    toString(mint)                      AS mint,
-    toString(b.amount)                  AS amount,
-    b.amount / pow(10, decimals)        AS value,
+    b.timestamp AS datetime,
+    toUnixTimestamp(b.timestamp) AS timestamp,
+    toString(program_id) AS program_id,
+    toString(account) AS token_account,
+    toString(mint) AS mint,
+    toString(b.amount) AS amount,
+    b.amount / pow(10, decimals) AS value,
+    if(
+        mint = 'So11111111111111111111111111111111111111111',
+        'SOL',
+        if(
+            mint = 'So11111111111111111111111111111111111111112',
+            'Wrapped SOL',
+            name
+        )
+    ) AS name,
+    if(
+        mint = 'So11111111111111111111111111111111111111111',
+        'SOL',
+        if(
+            mint = 'So11111111111111111111111111111111111111112',
+            'WSOL',
+            symbol
+        )
+    ) AS symbol,
     decimals,
-    {network_id:String}     AS network_id
+    {network_id:String} AS network_id
 FROM balances AS b FINAL
-WHERE ({token_account:String}     = '' OR owner = {token_account:String})
+LEFT JOIN metadata USING mint
+WHERE ({token_account:String}     = '' OR account = {token_account:String})
     AND ({mint:String}            = '' OR mint = {mint:String})
     AND ({program_id:String}      = '' OR program_id = {program_id:String})
     AND b.amount > 0
-ORDER BY owner, mint
+ORDER BY account, mint
 LIMIT  {limit:Int}
 OFFSET {offset:Int};
--- src/sql/transfers/svm.sql
+++ src/sql/transfers/svm.sql
@@ -1,31 +1,60 @@
-WITH t AS (
+WITH
+filtered_transfers AS (
     SELECT
-        timestamp_since_genesis,
+        block_num,
+        timestamp,
+        signature,
+        toString(program_id) AS program_id,
+        toString(authority) AS authority,
+        toString(mint) AS mint,
+        toString(source) AS source,
+        toString(destination) AS destination,
+        toString(amount) as amount,
         decimals,
-        *
+        transfers.amount / pow(10, ifNull(decimals, 0)) AS value,
+        {network_id: String} AS network_id
     FROM transfers
     WHERE timestamp BETWEEN {startTime: UInt64} AND {endTime: UInt64}
-    ORDER BY timestamp DESC
+        AND ({source:String}            = '' OR source = {source:String})
+        AND ({destination:String}       = '' OR destination = {destination:String})
+        AND ({mint:String}              = '' OR mint = {mint:String})
+        AND ({authority:String}         = '' OR authority = {authority:String})
+        AND ({program_id:String}         = '' OR program_id = {program_id:String})
+    LIMIT   {limit:int}
+    OFFSET  {offset:int}
 )
 SELECT
     block_num,
-    t.timestamp_since_genesis AS datetime,
-    toUnixTimestamp(t.timestamp_since_genesis) AS timestamp,
-    tx_hash AS signature,
-    toString(program_id) AS program_id,
-    toString(authority) AS authority,
-    toString(mint_raw) AS mint,
-    toString(source) AS source,
-    toString(destination) AS destination,
-    toString(amount) as amount,
+    timestamp AS datetime,
+    toUnixTimestamp(timestamp) AS timestamp,
+    signature,
+    program_id,
+    authority,
+    mint,
+    if(
+        mint = 'So11111111111111111111111111111111111111111',
+        'SOL',
+        if(
+            mint = 'So11111111111111111111111111111111111111112',
+            'Wrapped SOL',
+            name
+        )
+    ) AS name,
+    if(
+        mint = 'So11111111111111111111111111111111111111111',
+        'SOL',
+        if(
+            mint = 'So11111111111111111111111111111111111111112',
+            'WSOL',
+            symbol
+        )
+    ) AS symbol,
+    source,
+    destination,
+    amount,
     decimals,
-    t.amount / pow(10, ifNull(decimals, 0)) AS value,
-    {network_id: String} AS network_id
-FROM t
-WHERE   ({source:String}            = '' OR source = {source:String})
-    AND ({destination:String}       = '' OR destination = {destination:String})
-    AND ({mint:String}              = '' OR mint = {mint:String})
-    AND ({authority:String}         = '' OR authority = {authority:String})
-    AND ({program_id:String}         = '' OR program_id = {program_id:String})
-LIMIT   {limit:int}
-OFFSET  {offset:int}
+    value,
+    network_id
+FROM filtered_transfers
+LEFT JOIN metadata USING mint
+ORDER BY timestamp DESC;
