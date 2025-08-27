78ef22c0ca65351baf303af3787e8a29cef1e863
fschoell <fschoell@posteo.de>
2025-08-27 12:27:33 +0200

    calculate value for native tokens

--- src/sql/transfers/svm.sql
+++ src/sql/transfers/svm.sql
@@ -8,7 +8,12 @@ SELECT
     destination,
     amount,
     decimals,
-    t.amount / pow(10, ifNull(decimals, 0)) AS value,
+    t.amount /
+    CASE
+        WHEN mint = 'So11111111111111111111111111111111111111111' THEN pow(10, 9)
+        WHEN decimals IS NOT NULL THEN pow(10, decimals)
+        ELSE 1
+    END AS value,
     {network_id: String} AS network_id
 FROM transfers t
 WHERE   timestamp BETWEEN {startTime: UInt64} AND {endTime: UInt64}
@@ -16,6 +21,6 @@ WHERE   timestamp BETWEEN {startTime: UInt64} AND {endTime: UInt64}
     AND ({destination:String}       = '' OR destination = {destination:String})
     AND ({mint:String}              = '' OR mint = {mint:String})
     AND ({authority:String}         = '' OR authority = {authority:String})
-    AND ({program_id:String}         = '' OR program_id = {program_id:String})
+    AND ({program_id:String}        = '' OR program_id = {program_id:String})
 LIMIT   {limit:UInt64}
 OFFSET  {offset:UInt64}
