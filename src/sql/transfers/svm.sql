a961e2fd7e85c6f67b2687a06d0d96d670bed964
fschoell <fschoell@posteo.de>
2025-08-27 12:16:59 +0200

    update transfers query to v0.2.3

--- src/sql/transfers/svm.sql
+++ src/sql/transfers/svm.sql
@@ -1,28 +1,18 @@
-WITH t AS (
-    SELECT
-        timestamp_since_genesis,
-        decimals,
-        *
-    FROM transfers
-    WHERE timestamp BETWEEN {startTime: UInt64} AND {endTime: UInt64}
-    ORDER BY timestamp DESC
-)
 SELECT
     block_num,
-    t.timestamp_since_genesis AS datetime,
-    toUnixTimestamp(t.timestamp_since_genesis) AS timestamp,
-    tx_hash AS signature,
-    toString(program_id) AS program_id,
-    toString(authority) AS authority,
-    toString(mint_raw) AS mint,
-    toString(source) AS source,
-    toString(destination) AS destination,
-    toString(amount) as amount,
+    signature,
+    program_id,
+    authority,
+    mint,
+    source,
+    destination,
+    amount,
     decimals,
     t.amount / pow(10, ifNull(decimals, 0)) AS value,
     {network_id: String} AS network_id
-FROM t
-WHERE   ({source:String}            = '' OR source = {source:String})
+FROM transfers t
+WHERE   timestamp BETWEEN {startTime: UInt64} AND {endTime: UInt64}
+    AND ({source:String}            = '' OR source = {source:String})
     AND ({destination:String}       = '' OR destination = {destination:String})
     AND ({mint:String}              = '' OR mint = {mint:String})
     AND ({authority:String}         = '' OR authority = {authority:String})
