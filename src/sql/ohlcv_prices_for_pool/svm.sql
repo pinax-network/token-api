60620ce5da04834d614ccc33af2c0f30818c753e
fschoell <fschoell@posteo.de>
2025-08-28 14:26:58 +0200

    add Pool OHLC prices for Solana

--- src/sql/ohlcv_prices_for_pool/svm.sql
+++ src/sql/ohlcv_prices_for_pool/svm.sql
@@ -1,33 +1,34 @@
-WITH decimals AS (
-    SELECT
-        pool,
-        if(decimals0 = 0, 9, decimals0) AS decimals0,
-        if(decimals1 = 0, 9, decimals1) AS decimals1,
-        pow(10, -abs(decimals0 - decimals1)) AS decimals_factor
-    FROM ohlc_prices
-    WHERE pool = {pool: String}
-)
+WITH mint0 AS (SELECT mint, decimals
+               FROM `solana:solana-tokens@v0.2.3`.initialize_mint),
+     mint1 AS (SELECT mint, decimals
+               FROM `solana:solana-tokens@v0.2.3`.initialize_mint)
 SELECT
     if(
         toTime(toStartOfInterval(o.timestamp, INTERVAL {interval: UInt64} MINUTE)) = toDateTime('1970-01-02 00:00:00'),
         toDate(toStartOfInterval(o.timestamp, INTERVAL {interval: UInt64} MINUTE)),
         toStartOfInterval(o.timestamp, INTERVAL {interval: UInt64} MINUTE)
     ) AS datetime,
-    toString(pool) AS pool,
-    toString(token0) AS token0,
-    toString(token1) AS token1,
-    decimals_factor * argMinMerge(o.open0) AS open,
-    decimals_factor * quantileDeterministicMerge({high_quantile: Float32})(o.quantile0) AS high,
-    decimals_factor * quantileDeterministicMerge({low_quantile: Float32})(o.quantile0) AS low,
-    decimals_factor * argMaxMerge(o.close0) AS close,
-    sum(o.gross_volume0) AS volume,
-    uniqMerge(o.uaw) AS uaw,
-    sum(o.transactions) AS transactions,
-    {network_id: String} AS network_id
+
+    o.amm_pool AS pool,
+    o.mint0    AS token0,
+    o.mint1    AS token1,
+
+    floor(argMinMerge(open0) * pow(10, any(m0.decimals) - any(m1.decimals)), 2) AS open,
+    floor(quantileDeterministicMerge(0.99)(quantile0) * pow(10, any(m0.decimals) - any(m1.decimals)), 2)  AS high,
+    floor(quantileDeterministicMerge(0.01)(quantile0) * pow(10, any(m0.decimals) - any(m1.decimals)), 2)  AS low,
+    floor(argMaxMerge(close0) * pow(10, any(m0.decimals) - any(m1.decimals)), 2)                          AS close,
+
+    floor(sum(gross_volume0) / pow(10, any(m0.decimals)), 2)         AS volume0,
+    floor(sum(gross_volume1) / pow(10, any(m1.decimals)), 2)         AS volume1,
+    floor(sum(net_flow0) / pow(10, any(m0.decimals)), 2)             AS netflow0,
+    floor(sum(net_flow1) / pow(10, any(m1.decimals)), 2)             AS netflow1
+
 FROM ohlc_prices AS o
-JOIN decimals USING pool
-WHERE pool = {pool: String} AND timestamp BETWEEN {startTime: UInt64} AND {endTime: UInt64}
-GROUP BY token0, token1, decimals_factor, pool, datetime
+    JOIN mint0 m0 USING mint0 AS mint
+    JOIN mint1 m1 USING mint1 AS mint
+WHERE   pool = {pool: String}
+    AND timestamp BETWEEN {startTime: UInt64} AND {endTime: UInt64}
+GROUP BY token0, token1, amm_pool, datetime
 ORDER BY datetime DESC
 LIMIT   {limit:UInt64}
 OFFSET  {offset:UInt64}
