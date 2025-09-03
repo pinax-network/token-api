b750318509c664e0a6c0b0322c3ea0cc81ba12a9
Etienne Donneger <etienne@pinax.network>
2025-09-03 15:52:22 +0000

    Update SVM Balances SQL (#198)
    - Rename `owner` to `account`
    - Use CTE for filtering

--- src/sql/balances_for_account/svm.sql
+++ src/sql/balances_for_account/svm.sql
@@ -1,19 +1,25 @@
+WITH filtered_balances AS
+(
+    SELECT *
+    FROM balances
+    WHERE ({token_account:String}     = '' OR account = {token_account:String})
+        AND ({mint:String}            = '' OR mint = {mint:String})
+        AND ({program_id:String}      = '' OR program_id = {program_id:String})
+        AND amount > 0
+    ORDER BY program_id
+    LIMIT  {limit:UInt64}
+    OFFSET {offset:UInt64}
+)
 SELECT
     block_num,
     b.timestamp                         AS datetime,
     toUnixTimestamp(b.timestamp)        AS timestamp,
     toString(program_id)                AS program_id,
-    toString(owner)                     AS token_account,
+    toString(account)                     AS token_account,
     toString(mint)                      AS mint,
     toString(b.amount)                  AS amount,
     b.amount / pow(10, decimals)        AS value,
     decimals,
     {network_id:String}     AS network_id
-FROM balances AS b FINAL
-WHERE ({token_account:String}     = '' OR owner = {token_account:String})
-    AND ({mint:String}            = '' OR mint = {mint:String})
-    AND ({program_id:String}      = '' OR program_id = {program_id:String})
-    AND b.amount > 0
-ORDER BY owner, mint
-LIMIT  {limit:UInt64}
-OFFSET {offset:UInt64};
+FROM filtered_balances AS b
+ORDER BY timestamp DESC
