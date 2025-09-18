f222d64469c873259f62b3dbc3ed47e6f1b51bb0
Denis Carriere <carriere.denis@gmail.com>
2025-09-18 13:43:48 -0400

    reference pool_activity_summary

--- src/sql/supported_dexes/svm.sql
+++ src/sql/supported_dexes/svm.sql
@@ -1,16 +1,16 @@
 SELECT
     toString(program_id) AS program_id,
-    program_names(program_id) AS program_name,
+    program_names(p.program_id) AS program_name,
     toString(amm) AS amm,
-    program_names(amm) AS amm_name,
+    program_names(p.amm) AS amm_name,
     IF(program_id = amm, false, true) AS is_aggregator,
     sum(transactions) AS total_transactions
-FROM pool_activity_summary
+FROM pool_activity_summary AS p
 WHERE ({program_id:String} = '' OR program_id = {program_id:String})
     AND ({amm:String} = '' OR amm = {amm:String})
 GROUP BY
-    program_id,
-    amm
+    p.program_id,
+    p.amm
 ORDER BY total_transactions DESC
 LIMIT   {limit:UInt64}
 OFFSET  {offset:UInt64}
