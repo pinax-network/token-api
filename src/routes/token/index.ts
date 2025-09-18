6d78c8940e59180650da4c319542cde418b02e32
Denis Carriere <carriere.denis@gmail.com>
2025-09-18 14:11:30 -0400

    hide /owner

--- src/routes/token/index.ts
+++ src/routes/token/index.ts
@@ -4,7 +4,7 @@ import dexes from './dexes/index.js';
 import historical from './historical/index.js';
 import holders from './holders/index.js';
 import ohlc from './ohlc/index.js';
-import owner from './owner/index.js';
+// import owner from './owner/index.js';
 import pools from './pools/index.js';
 import swaps from './swaps/index.js';
 import tokens from './tokens/index.js';
@@ -13,14 +13,14 @@ import transfers from './transfers/index.js';
 const router = new Hono();
 
 router.route('/balances', balances);
-router.route('/dexes', dexes);
 router.route('/transfers', transfers);
 router.route('/tokens', tokens);
 router.route('/holders', holders);
 router.route('/swaps', swaps);
 router.route('/pools', pools);
 router.route('/ohlc', ohlc);
-router.route('/owner', owner);
+// router.route('/owner', owner);
 router.route('/historical', historical);
+router.route('/dexes', dexes);
 
 export default router;
--- src/sql/owner_for_account/svm.sql
+++ src/sql/owner_for_account/svm.sql
@@ -1,21 +1,7 @@
-WITH owners AS (
-    SELECT
-        max(timestamp) AS last_update,
-        max(block_num) AS last_update_block_num,
-        argMax(owner, o.block_num) AS owner
-    FROM owner_state_latest AS o
-    WHERE account = {account:String}
-    GROUP BY o.owner, account
-)
 SELECT
-    last_update,
-    last_update_block_num,
-    toUnixTimestamp(last_update) AS last_update_timestamp,
-    owner,
-    if((SELECT count() FROM owners) > 1, true, false) AS is_closed,
+    *,
     {network_id:String} AS network_id
-FROM owners
-WHERE owner != ''
-ORDER BY last_update DESC
+FROM accounts
+WHERE account = {account:String}
 LIMIT  {limit:UInt64}
 OFFSET {offset:UInt64}
