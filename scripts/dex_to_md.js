10f575452335a5c48b97d2e21e0d1d01f120354e
Denis Carriere <carriere.denis@gmail.com>
2025-09-26 10:28:34 -0400

    remove comments

--- scripts/dex_to_md.js
+++ scripts/dex_to_md.js
@@ -1,25 +1,5 @@
 const response = await (await fetch('http://localhost:8000/dexes/svm?limit=1000')).json();
 
-// # Programs and Aggregators
-
-// ## Non-Aggregators (`is_aggregator = false`)
-
-// | Program ID | Program Name |
-// |------------|--------------|
-// | P001       | Program Alpha |
-// | P002       | Program Beta  |
-// | P003       | Program Gamma |
-// | ...        | ...           |
-
-// ## Aggregators (`is_aggregator = true`)
-
-// | AMM | AMM Name     |
-// |-----|--------------|
-// | A001| Aggregator One |
-// | A002| Aggregator Two |
-// | A003| Aggregator Three |
-// | ... | ...            |
-
 // Non-Aggregators
 console.log('| Program Name | Program ID |');
 console.log('|------------|--------------|');
