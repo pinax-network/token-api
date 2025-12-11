06568dba9da5e85e4aea1a7c67fcceb40b51d0c3
Etienne Donneger <etienne@pinax.network>
2025-12-11 16:34:09 -0500

    Fix OpenAPI endpoint parameters not appearing in the schema
    Using only `zValidator` doesn't add automatically the parameters schema to the
    OpenAPI spec.
    
    The fix adds back `validator` from `hono-openapi` *after* the `zValidator` for
    having the proper schema.

--- src/routes/v1/evm/nft/collections/evm.ts
+++ src/routes/v1/evm/nft/collections/evm.ts
@@ -1,6 +1,6 @@
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
-import { describeRoute, resolver } from 'hono-openapi';
+import { describeRoute, resolver, validator } from 'hono-openapi';
 import { z } from 'zod';
 import { config } from '../../../../../config.js';
 import { handleUsageQueryError, makeUsageQueryJson } from '../../../../../handleQuery.js';
@@ -90,7 +90,7 @@ type ValidatedData = z.infer<typeof querySchema>;
 
 const route = new Hono<{ Variables: { validatedData: ValidatedData } }>();
 
-route.get('/', openapi, zValidator('query', querySchema, validatorHook), async (c) => {
+route.get('/', openapi, zValidator('query', querySchema, validatorHook), validator('query', querySchema), async (c) => {
     const params = c.req.valid('query');
 
     const dbConfig = config.nftDatabases[params.network];
--- src/routes/v1/evm/transfers/evm.ts
+++ src/routes/v1/evm/transfers/evm.ts
@@ -1,6 +1,6 @@
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
-import { describeRoute, resolver } from 'hono-openapi';
+import { describeRoute, resolver, validator } from 'hono-openapi';
 import { z } from 'zod';
 import { config } from '../../../../config.js';
 import { handleUsageQueryError, makeUsageQueryJson } from '../../../../handleQuery.js';
@@ -120,7 +120,7 @@ const openapi = describeRoute(
 
 const route = new Hono<{ Variables: { validatedData: z.infer<typeof querySchema> } }>();
 
-route.get('/', openapi, zValidator('query', querySchema, validatorHook), async (c) => {
+route.get('/', openapi, zValidator('query', querySchema, validatorHook), validator('query', querySchema), async (c) => {
     const params = c.req.valid('query');
 
     const dbConfig = config.tokenDatabases[params.network];
