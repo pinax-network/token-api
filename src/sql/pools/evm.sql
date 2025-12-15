4de772317cebd3a78a63a03a5443d497beb19803
Denis Carriere <carriere.denis@gmail.com>
2025-12-15 16:11:32 -0500

    Update pools

--- src/sql/pools/evm.sql
+++ src/sql/pools/evm.sql
@@ -1,50 +1,74 @@
 WITH has_pools AS (
     SELECT DISTINCT pool
     FROM state_pools_aggregating_by_token
+    WHERE token IN {input_token:Array(String)} OR token IN {output_token:Array(String)}
+),
+pools AS (
+    SELECT
+        pool,
+        factory,
+        protocol,
+        sum(transactions) as transactions
+    FROM state_pools_aggregating_by_pool
     WHERE
-        token IN {input_token:Array(String)} OR
-        token IN {output_token:Array(String)}
+        ({input_token:Array(String)} = [''] OR pool IN has_pools)
+    AND ({output_token:Array(String)} = [''] OR pool IN has_pools)
+    AND ({pool:Array(String)} = [''] OR pool IN {pool:Array(String)})
+    AND ({factory:Array(String)} = [''] OR factory IN {factory:Array(String)})
+    AND ({protocol:String} = '' OR toString(protocol) = {protocol:String})
+    GROUP BY pool, factory, protocol
+    ORDER BY transactions DESC
+    LIMIT   {limit:UInt64}
+    OFFSET  {offset:UInt64}
+),
+pools_with_tokens AS (
+    SELECT
+        pool,
+        factory,
+        protocol,
+        arraySort(groupArrayDistinct(token)) as tokens,
+        arrayElement(tokens, 1) AS token0,
+        arrayElement(tokens, 2) AS token1
+
+    FROM pools AS p
+    JOIN state_pools_aggregating_by_token AS pt ON p.pool = pt.pool
+    GROUP BY pool, factory, protocol
 )
 SELECT
-    /* DEX identity */
-    p.factory AS factory,
-    p.pool AS pool,
-
     /* initialize */
-    initialize_block_num AS block_num,
-    initialize_timestamp AS datetime,
-    initialize_tx_hash AS transaction_id,
+    i.block_num AS block_num,
+    i.timestamp AS timestamp,
+    i.tx_hash AS tx_hash,
+
+    /* DEX */
+    p.pool AS pool,
+    p.factory AS factory,
+    p.protocol AS protocol,
 
     /* tokens */
+    pt.tokens AS tokens,
+
     CAST ((
-        arrayElement(pt.tokens, 1),
+        pt.token0,
+        m0.symbol,
+        m0.decimals
+    ) AS Tuple(address String, symbol String, decimals UInt8)) AS input_token,
+
+    CAST ((
+        pt.token1,
         m1.symbol,
         m1.decimals
-    ) AS Tuple(address String, symbol String, decimals UInt8)) AS input_token,
-    CAST ((
-        arrayElement(pt.tokens, 2),
-        m2.symbol,
-        m2.decimals
     ) AS Tuple(address String, symbol String, decimals UInt8)) AS output_token,
 
-    fee,
-    toString(protocol) AS protocol,
-    {network:String} AS network
+    /* stats */
+    p.transactions AS transactions,
 
+    /* Fees */
+    f.fee AS fee
 FROM pools AS p
-WHERE
-    ({input_token:Array(String)} = [''] OR p.pool IN has_pools)
-AND ({output_token:Array(String)} = [''] OR p.pool IN has_pools)
-AND ({pool:Array(String)} = [''] OR pool IN {pool:Array(String)})
-AND ({factory:Array(String)} = [''] OR factory IN {factory:Array(String)})
-AND ({protocol:String} = '' OR toString(protocol) = {protocol:String})
-AND ({input_token:Array(String)} = [''] OR arrayElement(pt.tokens, 1) IN {input_token:Array(String)})
-AND ({output_token:Array(String)} = [''] OR arrayElement(pt.tokens, 2) IN {output_token:Array(String)})
-
-LEFT JOIN pools_tokens AS pt ON pt.pool = p.pool
-LEFT JOIN metadata AS m1 ON arrayElement(pt.tokens, 1) = m1.contract
-LEFT JOIN metadata AS m2 ON arrayElement(pt.tokens, 2) = m2.contract
-
+JOIN state_pools_initialize AS i ON p.pool = i.pool
+JOIN state_pools_fees AS f ON p.pool = f.pool
+JOIN pools_with_tokens AS pt ON p.pool = pt.pool
+JOIN metadata AS m0 ON pt.token0 = m0.contract
+JOIN metadata AS m1 ON pt.token1 = m1.contract
 ORDER BY p.transactions DESC
-LIMIT   {limit:UInt64}
-OFFSET  {offset:UInt64}
