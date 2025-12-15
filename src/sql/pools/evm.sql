688f962572040611284d2ac6d99907e0451a9869
Denis Carriere <carriere.denis@gmail.com>
2025-12-14 21:36:16 -0500

    Update pools (NOT FINISHED)

--- src/sql/pools/evm.sql
+++ src/sql/pools/evm.sql
@@ -1,88 +1,50 @@
-WITH filtered_pools AS (
-    SELECT
-        initialize_block_num AS block_num,
-        initialize_timestamp AS datetime,
-        initialize_tx_hash AS transaction_id,
-        toString(factory) AS factory,
-        pool,
-        arrayElement(tokens, 1) AS token0,
-        arrayElement(tokens, 2) AS token1,
-        fee,
-        protocol
-    FROM pools
+WITH has_pools AS (
+    SELECT DISTINCT pool
+    FROM state_pools_aggregating_by_token
     WHERE
-        ({pool:Array(String)} = [''] OR pool IN {pool:Array(String)})
-        AND ({factory:Array(String)} = [''] OR factory IN {factory:Array(String)})
-        AND ({input_token:Array(String)} = [''] OR hasAny(tokens, {input_token:Array(String)}))
-        AND ({output_token:Array(String)} = [''] OR hasAny(tokens, {output_token:Array(String)}))
-        AND ({protocol:String} = '' OR CAST(protocol AS String) = replaceAll({protocol:String}, '_', '-'))
-    ORDER BY datetime DESC
-    LIMIT   {limit:UInt64}
-    OFFSET  {offset:UInt64}
-),
-
-unique_contracts AS (
-    SELECT token0 AS contract FROM filtered_pools
-    UNION DISTINCT
-    SELECT token1 AS contract FROM filtered_pools
-),
-
-metadata AS (
-    SELECT
-        contract,
-        CAST(
-            (contract, symbol, name, decimals)
-            AS Tuple(address String, symbol Nullable(String), name Nullable(String), decimals Nullable(UInt8))
-        ) AS token,
-        symbol,
-        decimals
-    FROM {db_evm_tokens:Identifier}.metadata_view
-    WHERE contract NOT IN ('0x0000000000000000000000000000000000000000', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-    AND contract IN (SELECT contract FROM unique_contracts)
-
-    UNION ALL
-
-    SELECT
-        contract,
-        CAST(
-            (contract, native_symbol, 'Native', 18)
-            AS Tuple(address String, symbol Nullable(String), name Nullable(String), decimals Nullable(UInt8))
-        ) AS token,
-        native_symbol AS symbol,
-        18 AS decimals
-    FROM (
-        SELECT
-            contract,
-            multiIf(
-                {network:String} = 'mainnet', 'ETH',
-                {network:String} = 'arbitrum-one', 'ETH',
-                {network:String} = 'avalanche', 'AVAX',
-                {network:String} = 'base', 'ETH',
-                {network:String} = 'bsc', 'BNB',
-                {network:String} = 'polygon', 'POL',
-                {network:String} = 'optimism', 'ETH',
-                {network:String} = 'unichain', 'ETH',
-                'ETH'
-            ) AS native_symbol
-        FROM (
-            SELECT '0x0000000000000000000000000000000000000000' AS contract
-            UNION ALL
-            SELECT '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' AS contract
-        )
-    )
-    WHERE contract IN ('0x0000000000000000000000000000000000000000', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-    AND contract IN (SELECT contract FROM unique_contracts)
+        token IN {input_token:Array(String)} OR
+        token IN {output_token:Array(String)}
 )
-
 SELECT
+    /* DEX identity */
     p.factory AS factory,
     p.pool AS pool,
-    m1.token AS input_token,
-    m2.token AS output_token,
-    p.fee AS fee,
-    replaceAll(CAST(p.protocol AS String), '-', '_') AS protocol,
+
+    /* initialize */
+    initialize_block_num AS block_num,
+    initialize_timestamp AS datetime,
+    initialize_tx_hash AS transaction_id,
+
+    /* tokens */
+    CAST ((
+        arrayElement(pt.tokens, 1),
+        m1.symbol,
+        m1.decimals
+    ) AS Tuple(address String, symbol String, decimals UInt8)) AS input_token,
+    CAST ((
+        arrayElement(pt.tokens, 2),
+        m2.symbol,
+        m2.decimals
+    ) AS Tuple(address String, symbol String, decimals UInt8)) AS output_token,
+
+    fee,
+    toString(protocol) AS protocol,
     {network:String} AS network
-FROM filtered_pools AS p
-LEFT JOIN metadata AS m1 ON p.token0 = m1.contract
-LEFT JOIN metadata AS m2 ON p.token1 = m2.contract
-ORDER BY p.datetime DESC, p.protocol
+
+FROM pools AS p
+WHERE
+    ({input_token:Array(String)} = [''] OR p.pool IN has_pools)
+AND ({output_token:Array(String)} = [''] OR p.pool IN has_pools)
+AND ({pool:Array(String)} = [''] OR pool IN {pool:Array(String)})
+AND ({factory:Array(String)} = [''] OR factory IN {factory:Array(String)})
+AND ({protocol:String} = '' OR toString(protocol) = {protocol:String})
+AND ({input_token:Array(String)} = [''] OR arrayElement(pt.tokens, 1) IN {input_token:Array(String)})
+AND ({output_token:Array(String)} = [''] OR arrayElement(pt.tokens, 2) IN {output_token:Array(String)})
+
+LEFT JOIN pools_tokens AS pt ON pt.pool = p.pool
+LEFT JOIN metadata AS m1 ON arrayElement(pt.tokens, 1) = m1.contract
+LEFT JOIN metadata AS m2 ON arrayElement(pt.tokens, 2) = m2.contract
+
+ORDER BY p.transactions DESC
+LIMIT   {limit:UInt64}
+OFFSET  {offset:UInt64}
