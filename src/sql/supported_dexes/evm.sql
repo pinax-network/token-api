b4586c9d9dd5e5b5f5dcc7c518b7ed33df31b62c
Denis Carriere <carriere.denis@gmail.com>
2025-12-15 16:15:44 -0500

    Fix Supported DEXes

--- src/sql/supported_dexes/evm.sql
+++ src/sql/supported_dexes/evm.sql
@@ -1,27 +1,12 @@
-WITH all_dexes AS (
-    SELECT
-        replaceAll(CAST(protocol AS String), '-', '_') AS protocol,
-        factory,
-        sum(transactions) as transactions,
-        uniqMerge(uaw) as uaw,
-        max(timestamp) as last_activity
-    FROM ohlc_prices
-    WHERE interval_min = 10080
-    GROUP BY
-        protocol,
-        factory
-)
-SELECT * FROM all_dexes
+SELECT
+    protocol,
+    factory,
+    sum(transactions) as transactions,
+    max(max_timestamp) as last_activity,
+    uniqMerge(uniq_tx_from) as uaw
+FROM state_pools_aggregating_by_pool
+GROUP BY
+    protocol,
+    factory
 ORDER BY transactions DESC
 LIMIT 100
-
--- SELECT
---     toString(factory) AS factory,
---     protocol,
---     sum(uaw) AS uaw,
---     sum(transactions) AS transactions,
---     max(timestamp) as last_activity
--- FROM pool_activity_summary
--- GROUP BY
---     factory, protocol
--- ORDER BY transactions DESC, uaw DESC, protocol, factory
