8cc14712f6d49a0ecd6f3fdb507e61da11d5d7b8
Denis Carriere <carriere.denis@gmail.com>
2025-12-17 09:28:44 -0500

    add network to metadata

--- src/sql/swaps/evm.sql
+++ src/sql/swaps/evm.sql
@@ -179,6 +179,6 @@ SELECT
     /* network */
     {network:String} AS network
 FROM filtered_swaps AS s
-LEFT JOIN metadata AS m1 ON s.input_contract = m1.contract
-LEFT JOIN metadata AS m2 ON s.output_contract = m2.contract
+LEFT JOIN metadata AS m1 ON {network: String} = m1.network AND s.input_contract = m1.contract
+LEFT JOIN metadata AS m2 ON {network: String} = m2.network AND s.output_contract = m2.contract
 ORDER BY minute DESC, timestamp DESC, block_num DESC, log_ordinal DESC
