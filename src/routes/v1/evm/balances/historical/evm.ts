0f39c33e3cbeafa75e286e318dad0b493c03253e
Denis Carriere <carriere.denis@gmail.com>
2026-01-21 09:46:42 -0500

    update historical balances for EVM/TVM

--- src/routes/v1/evm/balances/historical/evm.ts
+++ src/routes/v1/evm/balances/historical/evm.ts
@@ -6,7 +6,7 @@ import { config } from '../../../../../config.js';
 import { handleUsageQueryError, makeUsageQueryJson } from '../../../../../handleQuery.js';
 import { injectSymbol } from '../../../../../inject/symbol.js';
 import { sqlQueries } from '../../../../../sql/index.js';
-import { EVM_CONTRACT_NATIVE_EXAMPLE } from '../../../../../types/examples.js';
+import { EVM_CONTRACT_USDT_EXAMPLE } from '../../../../../types/examples.js';
 import {
     apiUsageResponseSchema,
     createQuerySchema,
@@ -22,7 +22,7 @@ import { getDateMinusMonths, validatorHook, withErrorResponses } from '../../../
 const querySchema = createQuerySchema({
     network: { schema: evmNetworkIdSchema },
     address: { schema: evmAddressSchema },
-    contract: { schema: evmContractSchema, batched: true, default: '', meta: { example: EVM_CONTRACT_NATIVE_EXAMPLE } },
+    contract: { schema: evmContractSchema, batched: true, default: '', meta: { example: EVM_CONTRACT_USDT_EXAMPLE } },
     interval: { schema: intervalSchema, prefault: '1d', meta: { example: '1w' } },
     start_time: { schema: timestampSchema, prefault: getDateMinusMonths(1) },
     end_time: { schema: timestampSchema, prefault: '2050-01-01' },
