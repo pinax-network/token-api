c0d09ad0937dc5fd097f97e1808d379d0c2e743f
Denis Carriere <carriere.denis@gmail.com>
2026-01-23 15:13:01 -0500

    Update holders

--- src/sql/holders_for_contract/evm.sql
+++ src/sql/holders_for_contract/evm.sql
@@ -1,39 +1,36 @@
 WITH balances AS (
     SELECT
         address,
-        contract,
-        max(timestamp) AS timestamp,
-        max(b.block_num) AS block_num,
-        argMax(b.balance, b.block_num) AS balance
+        timestamp,
+        block_num,
+        balance
     FROM {db_balances:Identifier}.erc20_balances AS b
-    PREWHERE contract IN {contract:String}
-    GROUP BY contract, address
-    /* drop small holders here (also drops 0 if min_balance > 0) */
-    HAVING balance > 0
+    WHERE contract = {contract:String} AND balance != 0
 )
 SELECT
     /* timestamps */
-    b.timestamp AS last_update,
-    b.block_num AS last_update_block_num,
-    toUnixTimestamp(b.timestamp) AS last_update_timestamp,
+    max(b.timestamp) AS last_update,
+    max(b.block_num) AS last_update_block_num,
+    toUnixTimestamp(max(b.timestamp)) AS last_update_timestamp,
 
     /* identifiers */
     b.address AS address,
-    b.contract AS contract,
+    {contract:String} AS contract,
 
     /* amounts */
-    toString(b.balance) AS amount,
-    b.balance / pow(10, decimals) AS value,
+    toString(argMax(balance, b.block_num)) AS amount,
+    argMax(balance, b.block_num) / pow(10, m.decimals) AS value,
 
-    /* metadata */
-    name,
-    symbol,
-    decimals,
+    /* decimals and metadata */
+    m.name AS name,
+    m.symbol AS symbol,
+    m.decimals AS decimals,
 
     /* network */
     {network:String} as network
 FROM balances AS b
-LEFT JOIN metadata.metadata AS m FINAL ON m.network = {network:String} AND b.contract = m.contract
-ORDER BY b.balance DESC
+LEFT JOIN metadata.metadata AS m FINAL ON m.network = {network:String} AND {contract:String} = m.contract
+GROUP BY address, contract, m.name, m.symbol, m.decimals
+ORDER BY value DESC
 LIMIT {limit:UInt64}
 OFFSET {offset:UInt64}
