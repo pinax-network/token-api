95e5d9a05ee39b9c2430cf4182f798241ca6f792
Denis Carriere <carriere.denis@gmail.com>
2026-01-29 11:44:51 -0500

    add GROUP BY contract, address

--- src/sql/holders_for_contract/evm.sql
+++ src/sql/holders_for_contract/evm.sql
@@ -3,25 +3,25 @@ WITH
 balances AS (
     SELECT
         address,
-        argMax(balance, timestamp) AS amt,
+        argMax(balance, block_num) AS amt,
         max(timestamp) AS ts,
         max(block_num) AS bn
     FROM {db_balances:Identifier}.erc20_balances
     WHERE contract = {contract:String} AND balance > 0
-    GROUP BY address
+    GROUP BY contract, address
 )
 SELECT
     /* timestamps */
-    max(b.ts) AS last_update,
-    max(b.bn) AS last_update_block_num,
-    toUnixTimestamp(max(b.ts)) AS last_update_timestamp,
+    b.ts AS last_update,
+    b.bn AS last_update_block_num,
+    toUnixTimestamp(b.ts) AS last_update_timestamp,
 
     /* identifiers */
     b.address AS address,
     {contract:String} AS contract,
 
-    toString(any(b.amt)) AS amount,
-    any(b.amt) / pow(10, m.decimals) AS value,
+    toString(b.amt) AS amount,
+    b.amt / pow(10, m.decimals) AS value,
 
     /* decimals and metadata */
     m.name AS name,
@@ -32,7 +32,6 @@ SELECT
     {network:String} as network
 FROM balances AS b
 LEFT JOIN metadata.metadata AS m FINAL ON m.network = {network:String} AND {contract:String} = m.contract
-GROUP BY address, contract, m.name, m.symbol, m.decimals
-ORDER BY value DESC, address
+ORDER BY amt DESC, address
 LIMIT {limit:UInt64}
 OFFSET {offset:UInt64}
