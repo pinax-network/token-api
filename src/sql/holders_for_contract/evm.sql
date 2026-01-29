75ca140ca3047d1e224b6fbd3480191af6fa322d
Yaro Shkvorets <shkvorets@gmail.com>
2026-01-29 11:51:06 -0500

    Optimize holders endpoints with cutoff logic (#328)
    * optimize holders endpoints with cutoff logic
    
    * move ORDER BY in top_native
    
    * add GROUP BY contract, address
    
    * move offset to balances
    
    ---------
    
    Co-authored-by: Denis Carriere <carriere.denis@gmail.com>

--- src/sql/holders_for_contract/evm.sql
+++ src/sql/holders_for_contract/evm.sql
@@ -1,25 +1,30 @@
-WITH balances AS (
+WITH
+
+balances AS (
     SELECT
         address,
-        timestamp,
-        block_num,
-        balance
-    FROM {db_balances:Identifier}.erc20_balances AS b
-    WHERE contract = {contract:String} AND balance != 0
+        argMax(balance, block_num) AS amt,
+        max(timestamp) AS ts,
+        max(block_num) AS bn
+    FROM {db_balances:Identifier}.erc20_balances
+    WHERE contract = {contract:String} AND balance > 0
+    GROUP BY contract, address
+    ORDER BY amt DESC
+    LIMIT {limit:UInt64}
+    OFFSET {offset:UInt64}
 )
 SELECT
     /* timestamps */
-    max(b.timestamp) AS last_update,
-    max(b.block_num) AS last_update_block_num,
-    toUnixTimestamp(max(b.timestamp)) AS last_update_timestamp,
+    b.ts AS last_update,
+    b.bn AS last_update_block_num,
+    toUnixTimestamp(b.ts) AS last_update_timestamp,
 
     /* identifiers */
     b.address AS address,
     {contract:String} AS contract,
 
-    /* amounts */
-    toString(argMax(balance, b.block_num)) AS amount,
-    argMax(balance, b.block_num) / pow(10, m.decimals) AS value,
+    toString(b.amt) AS amount,
+    b.amt / pow(10, m.decimals) AS value,
 
     /* decimals and metadata */
     m.name AS name,
@@ -30,7 +35,3 @@ SELECT
     {network:String} as network
 FROM balances AS b
 LEFT JOIN metadata.metadata AS m FINAL ON m.network = {network:String} AND {contract:String} = m.contract
-GROUP BY address, contract, m.name, m.symbol, m.decimals
-ORDER BY value DESC, address
-LIMIT {limit:UInt64}
-OFFSET {offset:UInt64}
