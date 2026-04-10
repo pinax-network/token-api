bf5cf45ec532c4124756634a7bdc9e27db2094bc
Denis Carriere <carriere.denis@gmail.com>
2026-04-10 12:13:16 -0400

    Update SVM Native

--- src/routes/holders/svm_native.sql
+++ src/routes/holders/svm_native.sql
@@ -1,19 +1,37 @@
+/* filter out small balances below 5000 SOL */
+WITH accounts AS (
+    SELECT account FROM {db_balances:Identifier}.native_balances
+    WHERE amount > toUInt64(5000 * pow(10, 9))
+),
+/* get the latest balance for each account */
+balances AS (
+    SELECT account, argMax(amount, b.block_num) as amount, max(timestamp) as timestamp, max(block_num) as block_num
+    FROM {db_balances:Identifier}.native_balances b
+    WHERE account IN (SELECT account FROM accounts)
+    GROUP BY account
+    ORDER BY amount DESC, account
+    LIMIT {limit:UInt64}
+    OFFSET {offset:UInt64}
+),
+owners AS (
+    SELECT account, owner FROM {db_accounts:Identifier}.owner_state
+    WHERE account IN (SELECT account FROM balances)
+)
 SELECT
-    b.ts AS last_update,
-    b.bn AS last_update_block_num,
-    toUnixTimestamp(b.ts) AS last_update_timestamp,
+    timestamp AS last_update,
+    block_num AS last_update_block_num,
+    toUnixTimestamp(timestamp) AS last_update_timestamp,
 
     /* token */
     '11111111111111111111111111111111' AS program_id,
     'So11111111111111111111111111111111111111111' AS mint,
 
     /* owner */
-    if(notEmpty(ol.owner), ol.owner, Null) AS owner,
-    b.account AS token_account,
+    account AS token_account,
 
     /* amount */
-    b.amt AS amount,
-    b.amt / pow(10, 9) AS value,
+    amount,
+    amount / pow(10, 9) AS value,
 
     /* accounts */
     9 AS decimals,
@@ -23,30 +41,6 @@ SELECT
     'SOL' AS symbol,
 
     {network:String} AS network
-FROM (
-    SELECT account, argMax(amount, block_num) AS amt, max(timestamp) AS ts,
-        max(block_num) AS bn
-    FROM {db_balances:Identifier}.native_balances
-    WHERE amount > 4000 * pow(10, 9)
-    GROUP BY account
-    ORDER BY amt DESC, account DESC
-    LIMIT {limit:UInt64}
-    OFFSET {offset:UInt64}
-) AS b
-LEFT JOIN (
-    SELECT account, argMax(owner, version) AS owner
-    FROM {db_accounts:Identifier}.owner_state
-    WHERE account IN (
-        SELECT account FROM (
-            SELECT account, argMax(amount, block_num) AS amt
-            FROM {db_balances:Identifier}.native_balances
-            WHERE amount > 4000 * pow(10, 9)
-            GROUP BY account
-            ORDER BY amt DESC
-            LIMIT {limit:UInt64}
-            OFFSET {offset:UInt64}
-        )
-    )
-    GROUP BY account
-) AS ol ON b.account = ol.account
-ORDER BY b.amt DESC, b.account
+FROM balances AS b
+LEFT JOIN owners USING (account)
+ORDER BY amount DESC, account
